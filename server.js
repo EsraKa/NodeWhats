ed398db71cb43660ed08f9103eea1f13c3e068af
Frezy Mboumba <frezymboumba@Frezys-MacBook-Pro.local>
2017-01-17 21:37:49 +0100

    Fetching the users from DB

--- server.js
+++ server.js
@@ -2,26 +2,34 @@
  * Created by Esra on 16/01/2017.
  */
 
-var app = require('express')()
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server)
-var ent = require('ent')
-var fs = require('fs');
-var MongoClient = require('mongodb').MongoClient;
+ var app = require('express')()
+ var server = require('http').createServer(app)
+ var io = require('socket.io').listen(server)
+ var ent = require('ent')
+ var fs = require('fs');
+ var MongoClient = require('mongodb').MongoClient;
+ var url = 'mongodb://localhost:27017/test';
 
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + './views/index.html');
-});
+ app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/Public/views/index.html');
+ });
 
 
-MongoClient.connect("mongodb://localhost/tutoriel", function(error, db) {
-  if(error)
-    return funCallback(error);
+// MongoClient.connect(url, function(err, database) {
+//   if (err){
+//     console.log("Could not connect to the database");
+//   }else {
+//     console.log("Connection established to the database and server");
+//     collection = database.collection("Users");
+//
+//     collection.find({}).toArray(function(error,result){
+//       if (err){
+//         console.log("Could not fetch the users from the database");
+//       }else {
+//         console.log(result);
+//       }
+//     })
+//   }
+  server.listen(8000);
 
-  console.log("connecté a la base");
-});
-
-var port = process.env.PORT || 8000;
-server.listen(port, function() {
-    console.log("App is running on port " + port);
 });
