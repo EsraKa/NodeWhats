34782a85413679aacac5f424e4a91ddd53367fa7
EsraKa <esra.karakus@ynov.com>
2017-01-17 16:40:57 +0100

    modules

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 /**
  * Created by Esra on 16/01/2017.
  */
+
+var app = require('express')()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+var ent = require('ent')
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + './views/index.html');
+});
+
+
+MongoClient.connect("mongodb://localhost/tutoriel", function(error, db) {
+  if(error)
+    return funCallback(error);
+
+  console.log("connecté a la base");
+});
+
+
+server.listen(8080);
