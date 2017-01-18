4afde7c27e0bd4dc143262010c11c01b797a0076
EsraKa <esra.karakus@ynov.com>
2017-01-18 20:43:21 +0100

    texting

--- server.js
+++ server.js
@@ -2,17 +2,40 @@
  * Created by Esra on 16/01/2017.
  */
 
- var app = require('express')()
- var server = require('http').createServer(app)
- var io = require('socket.io').listen(server)
- var ent = require('ent')
- var fs = require('fs');
- var MongoClient = require('mongodb').MongoClient;
- var url = 'mongodb://localhost:27017/test';
+var app = require('express')()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+var ent = require('ent')
+var fs = require('fs');
+var moment = require('moment');
+// var MongoClient = require('mongodb').MongoClient;
+// var url = 'mongodb://localhost:27017/test';
 
- app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/Public/views/index.html');
- });
+
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/css/style.css', function(req, res) {
+  res.sendFile(__dirname + '/css/style.css');
+});
+
+
+io.sockets.on('connection', function(socket) {
+
+socket.on('nouveau_client', function (pseudo) {
+  pseudo = ent.encode(pseudo);
+  socket.pseudo = pseudo;
+  socket.broadcast.emit('nouveau_client', pseudo);
+});
+
+socket.on('message', function (message) {
+  message = ent.encode(message);
+  socket.broadcast.emit('message', {
+    pseudo: socket.pseudo,
+    message: message
+  });
+});
 
 
 // MongoClient.connect(url, function(err, database) {
@@ -30,6 +53,7 @@
 //       }
 //     })
 //   }
-  server.listen(8000);
 
 });
+  server.listen(8000);
+
