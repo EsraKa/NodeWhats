270b8b30f4bc424b509cff4dbc1f2a40d4e1382d
Omegadrien <omegadrien@gmail.com>
2017-01-27 11:34:05 +0100

    add upload systemgit status You have to create a folder public/upload/ to make it work.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-/**
- * Created by Esra on 20/01/2017.
- */
+// Variables used with the upload system
+var SelectedFile; 
+var FReader;
+
+
 var socket = io.connect('http://' + document.domain + ':'+ location.port);
 
 var pseudo = prompt('Quel est votre pseudo ?');
@@ -68,6 +70,15 @@ function addZero(i) {
   return i;
 }
 
+function onClickUpload() {
+  if(document.getElementById("UploadBox").style.visibility == "visible") {
+    document.getElementById("UploadBox").style.visibility='hidden';
+  }
+  else {
+    document.getElementById("UploadBox").style.visibility='visible';
+  }
+}
+
 function onClickButtonHappy(){
   var text = document.getElementById("message").value;
   document.getElementById("message").value = text + "😁";
@@ -86,3 +97,67 @@ function clickEmoji() {
     document.getElementById("emoji").style.visibility='visible';
   }
 }
+
+
+///// Upload files ////
+function StartUpload(){
+    if(document.getElementById('FileBox').value != "")
+    { 
+        FReader = new FileReader();
+        Name = SelectedFile.name;
+
+        var Content = ' <div class="progress" id=progressbar><div class="progress-bar" role="progressbar" aria-valuenow="0"aria-valuemin="0" aria-valuemax="100" style="width:0%">0%</div></div>'
+
+        Content += "<span id='Uploaded'> En cours de transfert... <span id='MB'>0</span> / " + Math.round(SelectedFile.size / 1048576) + " Mo</span>";
+        document.getElementById('UploadArea').innerHTML = Content;
+        FReader.onload = function(evnt){
+            socket.emit('Uploading', { 'Name' : Name, Data : evnt.target.result });
+        }
+        socket.emit('StartUpload', { 'Name' : Name, 'Size' : SelectedFile.size });
+    }
+    else {
+        alert("Veuillez choisir un fichier.");
+    }
+}
+
+
+window.addEventListener("load", Ready); //Once the page is loaded.
+
+function Ready(){ 
+    if(window.File && window.FileReader){ //These are the relevant HTML5 objects that we are going to use 
+        document.getElementById('FileBox').addEventListener('change', FileChosen);
+    }
+    else {
+        document.getElementById('UploadArea').innerHTML = "Veuillez mettre à jour votre navigateur Internet.";
+    }
+}
+
+function FileChosen(evnt) {
+    SelectedFile = evnt.target.files[0];
+}
+
+
+function UpdateBar(percent){
+  var MBDone = Math.round(((percent/100.0) * SelectedFile.size) / 1048576);
+  document.getElementById('MB').innerHTML = MBDone;
+  document.getElementById('progressbar').innerHTML = '<div class="progress-bar" role="progressbar" aria-valuenow="'+ (Math.round(percent*100)/100) +'"aria-valuemin="0" aria-valuemax="100" style="width:'+ (Math.round(percent*100)/100) +'%">'+ (Math.round(percent*100)/100) +'%</div>';
+}
+
+socket.on('MoreData', function (data){
+  UpdateBar(data['Percent']);
+  var Place = data['Place'] * 524288; //The Next Blocks Starting Position
+  var NewFile; //The Variable that will hold the new Block of Data
+  if(SelectedFile.slice) 
+      NewFile = SelectedFile.slice(Place, Place + Math.min(524288, (SelectedFile.size-Place)));
+  else
+      NewFile = SelectedFile.slice(Place, Place + Math.min(524288, (SelectedFile.size-Place)));
+  FReader.readAsBinaryString(NewFile);
+});
+
+socket.on('Done', function (data){
+    document.getElementById('UploadArea').innerHTML = "<p>Upload OK !</p>";
+    document.getElementById('message').value = 'http://' + document.domain + ':'+ location.port + "/" + data['Upload'];
+
+    document.getElementById("UploadArea").innerHTML = '<label for="FileBox">Choisissez un fichier à partager : </label><input type="file" id="FileBox"><br><button  type="button" id="UploadButton" class="Button" onclick="StartUpload()">Partager</button>';
+    document.querySelector('#FileBox').addEventListener('change', FileChosen); //To fix a bug. 
+});
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+app.use(express.static('public/upload'));
+
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var ent = require('ent');
 var path = require('path');
 var fs = require('fs');
 var mongoConn = require('./js/MongoConn');
+
+var Files = {};
 
 app.get('/', function(req, res) {
  res.sendFile(__dirname + '/views/index.html');
@@ -21,7 +27,6 @@ app.get('/js/app.js', function(req, res) {
 app.get('/views/video.html', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/video.html'));
 });
-
 
 io.sockets.on('connection', function(socket) {
 
@@ -40,8 +45,67 @@ io.sockets.on('connection', function(socket) {
     });
     mongoConn.postMessage(socket.pseudo,message)
   });
+
+
+  ////// BEGIN Upload //////
+  socket.on('StartUpload', function (data) { //data contains the variables that we passed through in the html file
+        var Name = data['Name'];
+        Files[Name] = {  //Create a new Entry in The Files Variable
+            FileSize : data['Size'],
+            Data     : "",
+            Downloaded : 0
+        }
+        var Place = 0;
+        try{
+            var Stat = fs.statSync('public/upload/' +  Name);
+            if(Stat.isFile())
+            {
+                Files[Name]['Downloaded'] = Stat.size;
+                Place = Stat.size / 524288;
+            }
+        }
+        catch(er){} //It's a New File
+        fs.open("public/upload/" + Name, "a", 0755, function(err, fd){
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                Files[Name]['Handler'] = fd; //We store the file handler so we can write to it later
+                socket.emit('MoreData', { 'Place' : Place, Percent : 0 });
+            }
+        });
 });
 
+  socket.on('Uploading', function (data){
+        var Name = data['Name'];
+        Files[Name]['Downloaded'] += data['Data'].length;
+        Files[Name]['Data'] += data['Data'];
+        if(Files[Name]['Downloaded'] == Files[Name]['FileSize']) //If File is Fully Uploaded
+        {   
+            fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
+                socket.emit('Done', {'Upload' : Name })
+            });
+        }
+        else if(Files[Name]['Data'].length > 10485760){ //If the Data Buffer reaches 10MB
+            fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
+                Files[Name]['Data'] = ""; //Reset The Buffer
+                var Place = Files[Name]['Downloaded'] / 524288;
+                var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
+                socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
+            });
+        }
+        else
+        {
+            var Place = Files[Name]['Downloaded'] / 524288;
+            var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
+            socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
+        }
+    });
+
+});
+////// END Upload //////
 
 var usr = [];
 io.on('connection', function(socket){
